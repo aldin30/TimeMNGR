89e7d67e49f10c93bd5c93a9cc359958a662425c
aldin30 <aldin30@gmail.com>
2025-12-31 21:11:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Vital for GitHub Pages subfolder hosting
+  base: './',
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
